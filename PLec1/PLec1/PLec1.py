45e671a83e4fb69220b06b04cc7b148a3b7ecbef
kbj627 <gbj627@gmail.com>
2015-09-02 13:46:08 +0900

    Lec1 Example
    문자열 예제

---
+++ PLec1/PLec1/PLec1.py
@@ -0,0 +1,55 @@
+﻿#program example 1
+#salary = int(input("please enter your salary : "))
+#bonus = int(input("please enter your bonus : "))
+#paycheck = salary + bonus
+#print(paycheck)
+
+#program example 2
+#data = float(input("enter number"))
+#print(round(data, 2))
+
+#program example 3
+#print("한글 출력하려면 고급 저장옵션에서 UTF-8형식 저장이 필요함")
+
+#program example 4
+#salary = int(input("Please enter your salary : "))
+#bonus = int(input("Please enter your bonus : "))
+#payCheck = salary + bonus
+#print(type(payCheck))
+
+#program example 5
+#name = "greenjoa"
+#print(name[0])
+#print(name[-2]*10)
+
+#program example 6
+#info = "201012345greenjoa"
+#sid = info[:9]      # 처음부터 9번째까지의 문자열
+#sname = info[9:]    # 9번째 다음부터 끝까지의 문자열
+#print(sid + " " + sname)
+
+#program example 7
+#str = "five"
+#a = "I eat %-10s apples." % str
+#print(a)
+
+#i = 98
+#a = "Error is %d%%." % i
+#print(a)
+
+#program example 8
+#str = "I ate {number} apples. so I was sick for {day}days." .format(number = 10, day=3)
+#print(str)
+
+#program example 9
+a = ","
+print(a.join("abcd"))
+a = "  hi"
+print(a.lstrip())
+a = "hi  "
+print(a.strip())
+
+#program exmaple 10
+#str = input("종료할까요?")
+#str = str.lower()
+#print(str)
