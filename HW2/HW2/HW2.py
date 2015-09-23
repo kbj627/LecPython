f6c32b3810871587ec5531c3d07f4ab6b647a0bb
kbj627 <gbj627@gmail.com>
2015-09-23 12:20:15 +0900

    HW2
    텍스트 파싱 및 엑셀파일 출력

---
+++ HW2/HW2/HW2.py
@@ -0,0 +1,44 @@
+﻿import os
+import xlsxwriter
+
+
+def readText(fileName) :
+    check = []
+    flag = True
+
+    with open(fileName, 'r') as file :
+        for part in file :
+            (feild1, feild2, feild3, feild4) = part.split('|', 3)
+            for val in check :
+                if val == feild3 :
+                    flag = False
+                    break;
+            if flag == True :
+                check.append(feild3)
+                print(feild3)
+            else :
+                flag = True
+    file.close()
+
+def createXlsx(t_fileName, str) :
+    row = 0
+    col = 0
+    workbook = xlsxwriter.Workbook(str+'.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    with open(t_fileName, 'r') as file :
+        for part in file :
+            (feild1, feild2, feild3, feild4) = part.split('|', 3)
+            if (feild3 == str.upper()) :
+                worksheet.write(row, col,     feild1)
+                worksheet.write(row, col + 1, feild2)
+                worksheet.write(row, col + 2, feild3)
+                worksheet.write(row, col + 3, feild4)
+                row += 1
+    workbook.close()
+
+
+readText('201401.txt')
+str = input('찾을 값을 입력해주세요.')
+createXlsx('201401.txt', str)
+print(str+'.xlsx 파일이 생성되었습니다.')
