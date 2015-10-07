0ca2b18ed91b080be3bd2ceecfdecbecc84891d6
kbj627 <gbj627@gmail.com>
2015-10-07 14:06:10 +0900

    PLec9
    내장함수 사용(sort 등)

--- HW3/HW3/HW3.py
+++ HW3/HW3/HW3.py
@@ -1,49 +1,65 @@
 ﻿import os
-import xlsxwriter
+try :
+    import xlsxwriter
+except ImportError:
+    if 'Y' == input('xlsxwriter가 없습니다. 설치 하시겠습니까?(y/n)').upper() :
+        os.system('pip install xlsxwriter')
+        import xlsxwriter
+    else :
+        os.sys.exit()
+
+class xlsxUser :
+    list = []
+    
+    
+    def __readText(self, fileName) :
+        check = []
+        flag = True
+
+        try :
+            with open(fileName, 'r') as file :
+                for part in file :
+                    (feild1, feild2, feild3, feild4, trash) = part.split('|', 4)
+                    self.list.append([feild1, feild2, feild3, feild4])
+                    for val in check :
+                        if val == feild3 :
+                            flag = False
+                            break;
+                    if flag == True :
+                        check.append(feild3)
+                        print(feild3)
+                    else :
+                        flag = True
+            file.close()
+        except FileNotFoundError:
+            print('File not found error')
+            return False
+
+        return True
 
 
-def readText(fileName) :
-    check = []
-    flag = True
+    def __writeXlsx(self, str) :
+        row = 0
+        col = 0
+        workbook = xlsxwriter.Workbook(str+'.xlsx')
+        worksheet = workbook.add_worksheet()
 
-    try :
-        with open(fileName, 'r') as file :
-            for part in file :
-                (feild1, feild2, feild3, feild4) = part.split('|', 3)
-                for val in check :
-                    if val == feild3 :
-                        flag = False
-                        break;
-                if flag == True :
-                    check.append(feild3)
-                    print(feild3)
-                else :
-                    flag = True
-        file.close()
-    except :
-        print('File not found')
-
-def createXlsx(t_fileName, str) :
-    row = 0
-    col = 0
-    workbook = xlsxwriter.Workbook(str+'.xlsx')
-    worksheet = workbook.add_worksheet()
-
-    with open(t_fileName, 'r') as file :
-        for part in file :
-            (feild1, feild2, feild3, feild4) = part.split('|', 3)
+        for part in self.list :
+            (feild1, feild2, feild3, feild4) = part
             if (feild3 == str.upper()) :
                 worksheet.write(row, col,     feild1)
                 worksheet.write(row, col + 1, feild2)
                 worksheet.write(row, col + 2, feild3)
                 worksheet.write(row, col + 3, feild4)
                 row += 1
-    file.close()
-    workbook.close()
+        workbook.close()
+
+    
+    def createXlsx(self, FIlename) :
+        if self.__readText(FIlename) == True :
+            str = input('찾을 값을 입력해주세요.')
+            self.__writeXlsx(str)
+            print(str + '.xlsx 파일이 생성되었습니다.')
 
 
-
-readText('201401.txt')
-str = input('찾을 값을 입력해주세요.')
-createXlsx('201401.txt', str)
-print(str+'.xlsx 파일이 생성되었습니다.')
+xlsxUser().createXlsx('201401.txt')
---
+++ PLec9/PLec9/PLec9.py
@@ -0,0 +1,57 @@
+﻿#program exam 1
+#def numcheck(l):
+#    return l%2 == 0
+#print(list(filter(numcheck, [0,1,2,3,4,5,6])))
+
+
+#program exam 2
+#a = 3
+#print(id(3))
+#print(id(a))
+
+
+#program exam 3
+#print(list(filter(lambda x : x % 2 == 0, [0,1,2,3,4,5,6])))
+
+
+#program exam 4
+#a = [1,2,3]
+#b = list(a)
+#c = a
+#print('a = ', id(a))
+#print('b = ', id(b))
+#print('c = ', id(c))
+
+
+#program exam 5
+#print(list(map(lambda a: a*2, [1,2,3,4])))
+#print(map(lambda a: a*2, [1,2,3,4]))
+
+
+#program exam 6
+#print(eval(repr("hi".upper())))
+#print(eval(str("hi".upper())))
+
+
+#program exam 7
+#list = [1, 2, 3, 5]
+#list2 = [123, 35, 12321, 545434, 'gfdsgfds', 'ASD', 'fffffff']
+
+#print(list.sort())
+#print(sorted(list))
+
+
+#program exam 8
+#print(list(zip([1,2,3], [4,5,6], [7,8,9])))
+#print(list(zip("abc", "def")))
+
+
+#program exam 9
+dic = {'홍길동' : [80, 70, 60, 92],
+        '김길동' : [24, 35, 18, 10],
+        '고길동' : [10, 20, 8 ,5] }
+
+for item in dic.keys():
+    dic[item].sort()
+    
+print(sorted(dic.items())) #정렬된 dic을 출력한다.
