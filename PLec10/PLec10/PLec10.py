c5bc00843454ed5f8b59e544ac3fbf3c354d085c
kbj627 <gbj627@gmail.com>
2015-10-12 19:16:36 +0900

    Plec10 보충
    외부함수(glob, tempfile, time, calendar, random, webbrower)

--- PLec10/PLec10/PLec10.py
+++ PLec10/PLec10/PLec10.py
@@ -63,8 +63,103 @@
 
 
 #program exam 6
-import os
+#import os
 
-print(os.path.dirname('C:\\python34/파일명.확장자'))
-print(os.path.expanduser('~\\파일명.확장자'))
-print(os.path.expandvars('$JAVA_HOME\\파일명.확장자'))
+#print(os.path.dirname('C:\\python34/파일명.확장자'))
+#print(os.path.expanduser('~\\파일명.확장자'))
+#print(os.path.expandvars('$JAVA_HOME\\파일명.확장자'))
+
+
+#program exam 7
+#import glob
+
+#print(glob.glob('./tes?.*'))
+#print(list(glob.iglob('*.*')))
+
+
+#program exam 8
+#import glob, os.path
+
+#ndir = nfile = 0
+
+#def traverse(dir, depth):
+#    global ndir, nfile
+#    for obj in glob.glob(dir + '/*'):
+#        if depth == 0:
+#            prefix = '|--'
+#        else:
+#            prefix = '|' + ' ' * depth + '|--'
+#        if os.path.isdir(obj):
+#            ndir += 1
+#            print(prefix + os.path.basename(obj))
+#            traverse(obj, depth+1)
+#        elif os.path.isfile(obj) :
+#            nfile +=1
+#            print(prefix + os.path.basename(obj))
+#        else:
+#            print(prefix + 'unknown object:',obj)
+
+#if __name__ == '__main__':
+#    traverse('..', 0)
+#    print('\n',ndir,'directories,',nfile, 'files')
+
+
+#program exam 9
+#import tempfile
+
+#with tempfile.TemporaryFile('w+') as fp:
+#    fp.write('Hello world!')
+#    fp.seek(0)
+#    data = fp.read()
+#    print(data)
+#    print(fp.name)
+
+
+#program exam 10
+#import time
+
+#print(time.strftime('%B %dth %A %H:%M', time.localtime()))
+#print(time.strftime('%Y-%m-%d %H:%M', time.localtime()))
+#print(time.localtime())
+
+#t1 = time.ctime(1234567)
+#print(time.strptime(t1))
+#print(time.strftime('%B %dth %A %H:%M', time.strptime(t1)))
+#print(time.strftime('%Y-%m-%d %H:%M', time.strptime(t1)))
+
+
+#program exam 11
+#import calendar
+
+#calendar.prmonth(2015, 10)
+#print(calendar.monthrange(1993,1))
+
+
+#program exam 12
+#import random
+
+#l = random.sample(range(100), 10)
+#random.shuffle(l)
+#print(random.choice(l))
+
+
+#program exam 13
+#import random
+
+#data = [('Red', 3), ('Blue', 1), ('Green', 4), ('Yellow', 2)]
+#datalist = []
+
+#for val, cnt in data:
+#    for i in range(cnt):
+#        datalist.append(val)
+#print(datalist)
+
+#datalist = [val for val, cnt in data for i in range(cnt)]
+#print(datalist)
+
+
+#program exam 14
+import webbrowser
+url = 'http://google.com'
+webbrowser.open_new_tab(url)
+webbrowser.open_new(url)
