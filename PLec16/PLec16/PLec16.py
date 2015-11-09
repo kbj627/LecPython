777af348dc876a0caba2fc8a370d30c8e293af9d
kbj627 <gbj627@gmail.com>
2015-11-09 20:09:15 +0900

    DB
    query 사용

---
+++ PLec16/PLec16/PLec16.py
@@ -0,0 +1,62 @@
+﻿#program exam 1
+import sqlite3
+from werkzeug import check_password_hash, generate_password_hash
+
+def get_DB():
+    db = sqlite3.connect("test.db")
+    return db
+
+def init_DB():
+    db = get_DB()
+    with open('schema.sql') as f:
+        db.cursor().executescript(f.read())
+        db.commit()
+    db.close()
+
+def register():
+    print("**** Register ****\n")
+    userid = input("id : ")
+    
+    db = get_DB()
+    cur = db.cursor()
+    cur.execute("select * from user where userid=?", [userid])
+    if cur.fetchone() != None :
+        print("중복")
+        return
+    
+    name = input("name : ")
+    userpw = input("password : ")
+    sql = "insert into user(userid, username, userpw) values(?,?,?);"
+    cur.execute(sql, [userid, name, generate_password_hash(userpw)])
+    cur.execute("select * from user;")
+    print(cur.fetchall())
+    db.commit()
+    db.close()
+    
+
+def login():
+    print("**** Login ****\n")
+    userid = input("id : ")
+    name = input("name : ")
+    userpw = input("password : ")
+    
+    db = get_DB()
+    cur = db.cursor()
+    cur.execute("select * from user where userid=?", [userid])
+    temp = cur.fetchone()
+    if temp != None :
+        print("ID가 존재합니다.")
+        return
+    elif check_password_hash(temp[3], userpw):
+        print("Login 성공")
+        return
+    else:
+        print("Password 틀림")
+        return
+    
+    db.commit()
+    db.close()
+
+init_DB()
+register()
+login()
---
+++ Team_homework/Team_homework/Team_homework.py
@@ -0,0 +1,43 @@
+﻿import sqlite3
+import webbrowser
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.parse import urljoin
+
+class crawler:
+    def crawl(self, pages, depth=2):
+        for i in range(depth):  #depth만큼 깊이를 탐색
+            newpage= set()
+            for page in pages:  #pages의 목록을 탐색
+                try:
+                    c = urlopen(page)
+                except:
+                    print("Could not open %s" % page)
+                    continue
+                soup = BeautifulSoup(c.read(), from_encoding="utf-8")
+                print('Found %s' % page)
+            links = soup('a')
+            for link in links:  #links에는 page의 url을 검색한 결과중 'a' 태그가 들어간 것 들이 저장됨
+                print(link)
+                if 'href' in dict(link.attrs):  #link의 attribute중 href가 있는 것
+                    url= urljoin(page, link['href'])    #urljoin의 기능은 현재 page에서 href가 되는 결과를 얻기위한 url로 만들어주는것
+                    if url.find("'")!=-1 : continue     #'를 찾은 결과가 -1이 아니면 ('가 있으면 안된다는 뜻인가?)
+                    url= url.split("#")[0]              # #이 있으면 자르고 왼쪽거
+                    if url[0:4]=='http':                # http가 있는 경우만 추가
+                        newpage.add(url)
+            pages = newpage
+
+def getMaxPage(url):
+    l = []
+    data = urlopen(url)
+    soup = BeautifulSoup(data.read(), from_encoding = 'utf-8')
+    for link in soup('a'):
+        if 'href' in dict(link.attrs):
+            if link['href'].find('page=') != -1:
+                l.append(link['href'].split('page=')[1])
+    page = max(l)
+
+    return page
+
+url = 'http://musicbrainz.org/area/b9f7d640-46e8-313e-b158-ded6d18593b3/artists?page=1'
+getMaxPage(url)
