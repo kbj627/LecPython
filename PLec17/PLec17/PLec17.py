716bf1b5cac0c4f9c82457da81bb5122898f4a94
kbj627 <gbj627@gmail.com>
2015-11-18 13:52:01 +0900

    flask 사용
    virtualenv 사용해서 가상 환경에 flask 설치해서 사용

---
+++ PLec17/PLec17/PLec17.py
@@ -0,0 +1,20 @@
+﻿#program exam 1
+from flask import Flask, url_for #the name of the application package
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+#    return 'Hello World!'
+    with app.test_request_context():
+        return redirect(url_for('login'))
+
+@app.route('/login/')
+def login():
+    return 'log_in'
+
+if __name__ == '__main__':
+    app.run()
+
+app.debug = True #소스 변경과 디버거 제공
+app.run(host = '172.16.39.21', port = 5000)
---
+++ PLec18/PLec18/PLec18.py
@@ -0,0 +1,20 @@
+﻿#program exam 1
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    data = [dict(href='http://www.naver.com', caption='네이버'),
+            dict(href='http://www.google.com', caption='구글'),
+            dict(href='http://www.daum.net', caption='다음')]
+    data2 = {'title' : 'Hello',
+             'name' : 'greenjoa'}
+#    return render_template('hello.html', name=name, items = data)
+#    return render_template('hello.html', **data2)
+    return render_template('main.html', name=name)
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run()
